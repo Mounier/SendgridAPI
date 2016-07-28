2ce250e97e5363868e802b168c7f52033876f49d
mounier <dridri_du91@hotmail.fr>
2016-07-28 15:07:06 +0200

    ajout d'un gradle

--- src/fr/sendgrid/api2/service/RecipientService.java
+++ src/fr/sendgrid/api2/service/RecipientService.java
@@ -185,13 +185,15 @@ public class RecipientService {
 			Request request = new Request();
 			request.method = Method.GET;
 			request.endpoint = "suppression/bounces";
-			Map<String, String> queryParams = new HashMap<String, String>();
-			Calendar now = Calendar.getInstance();
+			Map<String,String> queryParams = new HashMap<String, String>();
+		    Calendar now = Calendar.getInstance();
 			long nowInSeconde = now.getTimeInMillis()/1000l;	// on divise par mille car time.getTIme() est en seconde or on veut des milli secondes
-			long oneMonthAgoInSeconde = (now.getTimeInMillis()/1000l) - 2588339; 	// on soustrait la valeur du temps now par 2588339 qui correspond environ à 1 mois en millisecondes pour nous ramener 1 mois avant
-			queryParams.put("start_time", String.valueOf(oneMonthAgoInSeconde));
+			long beforeOneMonthInSeconde = (now.getTimeInMillis()/1000l) - 2588339;		// on soustrait la valeur du temps now par 2588339 qui correspond environ à 1 mois en millisecondes pour nous ramener 1 mois avant
+			queryParams.put("start_time", String.valueOf(beforeOneMonthInSeconde));
 			queryParams.put("end_time", String.valueOf(nowInSeconde));
-			request.queryParams = queryParams;
+//		    queryParams.put("offset", "0");		// 0 pour le début de la liste
+//		    queryParams.put("limit", "300");	// 300 pour indiquer l'indice de la position ou on veut s'arreter
+		    request.queryParams = queryParams;
 			Response responseSpam = sg.api(request);
 			System.out.println(responseSpam.statusCode);
 			System.out.println(responseSpam.body);
